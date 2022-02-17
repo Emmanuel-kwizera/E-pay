b8ea4216dd13c196ed238198684a8d38f7e233cf
Kwizera Emmanuel <51673007+Emmanuel-kwizera@users.noreply.github.com>
2022-02-17 16:34:46 +0200

    Update Buy.jsx

--- Front-End/src/views/Buy.jsx
+++ Front-End/src/views/Buy.jsx
@@ -47,18 +47,18 @@ const Buy = () => {
 
   return (
     <React.Fragment>
-      <Header title="Buy" />
+      <Header title="With this page pay for your eclecricity token" />
       <main>
         <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
           <div className="flex items-center justify-center">
             <div className="w-full max-w-md pt-7">
-              <form className="bg-white shadow-lg rounded px-12 pt-6 pb-8 mb-4">
-                <div className="text-gray-800 text-2xl flex justify-center border-b-2 py-2 mb-4">
-                  T-seller
+              <form className="bg-white rounded px-12 pt-6 pb-8 mb-4 border-2 border-zinc-700">
+                <div className="text-xl mt-1 mb-5 font-bold text-center text-[#dc143c]">
+                  E-Pay
                 </div>
                 <div className="mb-4">
                   <label
-                    className="block text-gray-700 text-sm font-normal mb-2"
+                    className="text-gray-600 block"
                     htmlFor="meterNumber"
                   >
                     Meter number
@@ -66,7 +66,7 @@ const Buy = () => {
                   <input
                     value={meter.meterNumber}
                     onChange={(e) => handleChange(e.target)}
-                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                    className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                     name="meterNumber"
                     type="number"
                     pattern="[0-8]*"
@@ -87,7 +87,7 @@ const Buy = () => {
                     Amount
                   </label>
                   <input
-                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+                    className="appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
                     placeholder="Amount"
                     name="amount"
                     type="number"
@@ -102,7 +102,7 @@ const Buy = () => {
                 </div>
 
                 <button
-                  className="px-4 py-2 rounded text-white inline-block shadow-lg bg-blue-500 hover:bg-blue-600 focus:bg-blue-700"
+                 className="bg-main focus:outline-none w-10/12 text-white p-3 px-5 rounded-3xl mt-2"
                   type="submit"
                 >
                   Buy
